5a059e25fdbe24677b07803aab7a572b73ecc438
osmanbozoglu <iosmanbozoglu@gmail.com>
2022-04-21 00:38:29 +0300

    AppUserService was implemented

---
+++ src/main/java/com/userservice/userservice/service/impl/AppUserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.userservice.userservice.service.impl;
+
+import com.userservice.userservice.models.AppUser;
+import com.userservice.userservice.models.Role;
+import com.userservice.userservice.repository.AppUserRepository;
+import com.userservice.userservice.repository.RoleRepository;
+import com.userservice.userservice.service.AppUserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class AppUserServiceImpl implements AppUserService {
+    private final AppUserRepository appUserRepository;
+    private final RoleRepository roleRepository;
+
+    @Override
+    public AppUser saveAppUser(AppUser user) {
+        log.info("Saving new user {} to the database", user.getName());
+        return appUserRepository.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
+        AppUser appUser = appUserRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+        appUser.getRoles().add(role);
+    }
+
+    @Override
+    public AppUser getAppUser(String username) {
+        log.info("Fetching user {}", username);
+        return appUserRepository.findByUsername(username);
+    }
+
+    @Override
+    public List<AppUser> getAppUsers() {
+        log.info("Fetching all users");
+        return appUserRepository.findAll();
+    }
+}
