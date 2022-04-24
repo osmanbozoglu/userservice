f294b8494c715c195fdff4878d27876a0efdd05d
osmanbozoglu <iosmanbozoglu@gmail.com>
2022-04-24 04:52:27 +0300

    Simple security has been implemented.

--- src/main/java/com/userservice/userservice/UserserviceApplication.java
+++ src/main/java/com/userservice/userservice/UserserviceApplication.java
@@ -1,7 +1,16 @@
 package com.userservice.userservice;
 
+import com.userservice.userservice.models.AppUser;
+import com.userservice.userservice.models.Role;
+import com.userservice.userservice.service.AppUserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -10,4 +19,22 @@ public class UserserviceApplication {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner runner(AppUserService appUserService){
+		return args -> {
+			appUserService.saveRole(new Role(null, "ROLE_USER"));
+			appUserService.saveRole(new Role(null, "ROLE_USER_ADMIN"));
+			appUserService.saveRole(new Role(null, "ROLE_USER_MANAGER"));
+			appUserService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			appUserService.saveAppUser(new AppUser(null, "Osman Bozoglu", "osmanbozoglu", "password123", new ArrayList<>()));
+
+			appUserService.addRoleToUser("osmanbozoglu", "ROLE_SUPER_ADMIN");
+		};
+	}
 }
--- src/main/java/com/userservice/userservice/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/userservice/userservice/service/impl/AppUserServiceImpl.java
@@ -7,22 +7,49 @@ import com.userservice.userservice.repository.RoleRepository;
 import com.userservice.userservice.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class AppUserServiceImpl implements AppUserService {
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
     private final AppUserRepository appUserRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByUsername(username);
+        if(null == appUser){
+            log.error("User not found!");
+            throw new UsernameNotFoundException("User not found!");
+        } else{
+            log.info("User found {}", username);
+
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        appUser.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new User(appUser.getUsername(), appUser.getPassword(), authorities);
+    }
 
     @Override
     public AppUser saveAppUser(AppUser user) {
         log.info("Saving new user {} to the database", user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return appUserRepository.save(user);
     }
 
